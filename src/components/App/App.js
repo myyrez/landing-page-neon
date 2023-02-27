656cac00b346f4f8291a9a42886e79bd5d5bdbfc
mathêe <matheusfc2626@gmail.com>
2023-02-27 20:05:02 -0300

    bugs corrigidos, ocultar senha pronto

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
+import { FiEyeOff, FiEye } from 'react-icons/fi'
 
 export const App = () => {
   const loginFundo = document.getElementById('loginFundo')
   const loginImagem = document.getElementById('loginImagem')
   const cadastroFundo = document.getElementById('cadastroFundo')
   const cadastroImagem = document.getElementById('cadastroImagem')
+  const senhaLogin = document.getElementById('senhaLogin')
+  const senhaCadastro = document.getElementById('senhaCadastro')
+  const confirmarSenhaCadastro = document.getElementById('confirmarSenhaCadastro')
 
   const [loginIndex, setLoginIndex] = useState(-1)
   const [cadastroIndex, setCadastroIndex] = useState(-1)
+  const [senhaOnOff, setSenhaOnOff] = useState(0)
 
+  const changePageLogin = () => {
+    setTimeout(() => {
+      document.body.style.transform = 'translateX(0vw)'
+    }, 500);
+
+      cadastroImagem.style.backgroundPosition = 'center 10%'
+      loginImagem.style.backgroundPosition = 'center 10%'
+      cadastroFundo.style.backgroundPosition = '0% 0%'
+      loginFundo.style.backgroundPosition = '0% 0%'
+      setLoginIndex(-1)
+      setCadastroIndex(-1)
+  }
+  const changePageCadastro = () => {
+    setTimeout(() => {
+      document.body.style.transform = 'translateX(-100vw)'
+    }, 500);
+
+      cadastroImagem.style.backgroundPosition = 'center 10%'
+      loginImagem.style.backgroundPosition = 'center 10%'
+      cadastroFundo.style.backgroundPosition = '0% 0%'
+      loginFundo.style.backgroundPosition = '0% 0%'
+      setLoginIndex(-1)
+      setCadastroIndex(-1)
+  }
+
+  const showPassword = () => {
+    if (senhaOnOff === 0) {
+      setSenhaOnOff(1)
+      senhaLogin.type = ''
+      senhaCadastro.type = ''
+      confirmarSenhaCadastro.type = ''
+    }
+    if (senhaOnOff === 1) {
+      setSenhaOnOff(0)
+      senhaLogin.type = 'password'
+      senhaCadastro.type = 'password'
+      confirmarSenhaCadastro.type = 'password'
+    } 
+  }
   
   useEffect(() => {
     Array.from(document.getElementsByClassName(styles.input))
@@ -66,12 +110,15 @@ export const App = () => {
         </div>
 
         <div className={styles.loginItens} id='loginItens'>
-          <input className={styles.input} placeholder='Email'/>
-          <input className={styles.input} placeholder='Senha'/>
+          <input className={styles.input} type='email' placeholder='Email'/>
+          <input className={styles.input} type='password' id='senhaLogin' placeholder='Senha'></input>
+          {senhaOnOff === 0 
+          ? <FiEyeOff className={styles.eyeIcon} onClick={showPassword}/>
+          : <FiEye className={styles.eyeIcon} onClick={showPassword}/>}
         </div>
 
         <div className={styles.cadastroDiv}>
-          <p className={styles.cadastroLink}>Não possui conta? Cadastre-se aqui.</p>
+          <p className={styles.cadastroLink} id='loginLink' onClick={changePageCadastro}>Não possui conta? Cadastre-se aqui.</p>
           <div className={styles.decoration}/>
         </div>
 
@@ -93,9 +140,20 @@ export const App = () => {
           <div className={styles.cadastroItens} id='cadastroItens'>
             <input className={styles.cadastroInput} placeholder='Nome'/>
             <input className={styles.cadastroInput} placeholder='Email'/>
-            <input className={styles.cadastroInput} placeholder='Senha'/>
-            <input className={styles.cadastroInput} placeholder='Confirmar senha'/>
+            <input className={styles.cadastroInput} type='password' id='senhaCadastro' placeholder='Senha'/>
+            <input className={styles.cadastroInput} type='password' id='confirmarSenhaCadastro' placeholder='Confirmar senha'/>
+            {senhaOnOff === 0 
+          ? <FiEyeOff className={styles.eyeIconCadastro} onClick={showPassword}/>
+          : <FiEye className={styles.eyeIconCadastro} onClick={showPassword}/>}
           </div>
+        </div>
+
+        <div className={styles.cadastroButtonDiv}>
+          <div className={styles.linkDiv}>
+            <p className={styles.cadastroLink} id='cadastroLink' onClick={changePageLogin}>Já possuo uma conta.</p>
+            <div className={styles.decoration}/>
+          </div>
+          <button className={styles.cadastroButton}>Cadastrar</button>
         </div>
 
         <div className={styles.cadastroFundo} id='cadastroFundo'/>
