baaf7dc7c08eedae24c37fa61d456a00c35ed9ed
mathêe <matheusfc2626@gmail.com>
2023-02-27 02:59:54 -0300

    problema do parallax arrumado, só finalizar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 
 export const App = () => {
-  const login = document.getElementById('login')
-  const cadastro = document.getElementById('cadastro')
+  const loginFundo = document.getElementById('loginFundo')
+  const loginImagem = document.getElementById('loginImagem')
+  const cadastroFundo = document.getElementById('cadastroFundo')
+  const cadastroImagem = document.getElementById('cadastroImagem')
 
+  const [loginIndex, setLoginIndex] = useState(-1)
+  const [cadastroIndex, setCadastroIndex] = useState(-1)
+
+  
   useEffect(() => {
     Array.from(document.getElementsByClassName(styles.input))
     .forEach((item, index) => {
       item.onmouseover = () => {
-        login.dataset.activeIndex = index;
+        setLoginIndex(index)
       }
     })
   
     Array.from(document.getElementsByClassName(styles.cadastroInput))
     .forEach((item, index) => {
       item.onmouseover = () => {
-        cadastro.dataset.activeIndex = index;
+        setCadastroIndex(index)
       }
     })
-  })
 
+    switch (loginIndex) {
+      case 0: 
+        loginFundo.style.backgroundPosition = '0% -25%'
+        loginImagem.style.backgroundPosition = 'center 10%'
+        break;
+      case 1:
+        loginFundo.style.backgroundPosition = '0% -50%'
+        loginImagem.style.backgroundPosition = 'center 20%'
+        break;
+    }
+
+    switch (cadastroIndex) {
+      case 0: 
+        cadastroFundo.style.backgroundPosition = '0% -25%'
+        cadastroImagem.style.backgroundPosition = 'center 10%'
+        break;
+      case 1:
+        cadastroFundo.style.backgroundPosition = '0% -50%'
+        cadastroImagem.style.backgroundPosition = 'center 20%'
+        break;
+      case 2:
+        cadastroFundo.style.backgroundPosition = '0% -75%'
+        cadastroImagem.style.backgroundPosition = 'center 30%'
+        break;
+      case 3:
+        cadastroFundo.style.backgroundPosition = '0% -100%'
+        cadastroImagem.style.backgroundPosition = 'center 40%'
+        break;
+    }
+  })
 
   return (
     <div className={styles.background}>
@@ -44,8 +79,8 @@ export const App = () => {
           <button className={styles.loginButton}>Entrar</button>
         </div>
 
-        <div className={styles.loginFundo}/>
-        <div className={styles.loginImagem}/>
+        <div className={styles.loginFundo} id='loginFundo'/>
+        <div className={styles.loginImagem} id='loginImagem'/>
       </div>
 
       <div className={styles.cadastro} id='cadastro'>
@@ -54,15 +89,17 @@ export const App = () => {
           <h1 className={styles.cadastroHeader}>Mirror.</h1>
         </div>
 
-        <div className={styles.cadastroItens} id='cadastroItens'>
-          <input className={styles.cadastroInput} placeholder='Nome'/>
-          <input className={styles.cadastroInput} placeholder='Email'/>
-          <input className={styles.cadastroInput} placeholder='Senha'/>
-          <input className={styles.cadastroInput} placeholder='Confirmar senha'/>
+        <div className={styles.cadastroItensWrap}>
+          <div className={styles.cadastroItens} id='cadastroItens'>
+            <input className={styles.cadastroInput} placeholder='Nome'/>
+            <input className={styles.cadastroInput} placeholder='Email'/>
+            <input className={styles.cadastroInput} placeholder='Senha'/>
+            <input className={styles.cadastroInput} placeholder='Confirmar senha'/>
+          </div>
         </div>
 
-        <div className={styles.cadastroFundo}/>
-        <div className={styles.cadastroImagem}/>
+        <div className={styles.cadastroFundo} id='cadastroFundo'/>
+        <div className={styles.cadastroImagem} id='cadastroImagem'/>
       </div>
     </div>
   )
